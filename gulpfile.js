3d672f92076b9401678e6b0b8cd4dbab78f9dda0
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2018-12-28 12:20:55 +0100

    load font css asynchronously, inline css

--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,96 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCss = require('gulp-clean-css');
-const connect = require('gulp-connect');
-const del = require('del');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCss = require('gulp-clean-css')
+const inlineSource = require('gulp-inline-source')
+const connect = require('gulp-connect')
+const del = require('del')
+const { resolve } = require('path')
 
 const dirs = {
-  src: 'src',
-  dest: 'dist'
-};
+  src: resolve(__dirname, 'src'),
+  dest: resolve(__dirname, 'dist'),
+}
 
 const html = {
-  entry: `${dirs.src}/index.html`,
-  dest: dirs.dest
-};
+  entry: resolve(dirs.src, 'index.html'),
+  dest: dirs.dest,
+  output: resolve(dirs.dest, 'index.html'),
+}
 
 const styles = {
-  entry: `${dirs.src}/styles/main.scss`,
-  all: `${dirs.src}/styles/**/*.scss`,
-  dest: `${dirs.dest}/css`
-};
+  entry: resolve(dirs.src, 'styles/main.scss'),
+  all: resolve(dirs.src, 'styles/**/*.scss'),
+  dest: resolve(dirs.dest, 'css'),
+}
 
 gulp.task('styles', () => {
-  gulp.src(styles.entry)
-    .pipe(sass({
-      includePaths: [
-        'node_modules',
-      ]
-    }))
+  return gulp
+    .src(styles.entry)
+    .pipe(
+      sass({
+        includePaths: ['node_modules'],
+      }),
+    )
     .pipe(autoprefixer())
     .pipe(gulp.dest(styles.dest))
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+})
 
 gulp.task('styles:production', () => {
-  gulp.src(styles.entry)
-    .pipe(sass({
-      includePaths: [
-        'node_modules',
-      ]
-    }))
+  return gulp
+    .src(styles.entry)
+    .pipe(
+      sass({
+        includePaths: ['node_modules'],
+      }),
+    )
     .pipe(autoprefixer())
     .pipe(cleanCss())
-    .pipe(gulp.dest(styles.dest));
-});
+    .pipe(gulp.dest(styles.dest))
+})
 
 gulp.task('html', () => {
-  gulp.src(html.entry)
+  return gulp
+    .src(html.entry)
     .pipe(gulp.dest(html.dest))
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+})
 
 gulp.task('html:production', () => {
-  gulp.src(html.entry)
-    .pipe(gulp.dest(html.dest));
-});
+  return gulp.src(html.entry).pipe(gulp.dest(html.dest))
+})
+
+gulp.task('html:inline-styles', ['html:production', 'styles:production'], () => {
+  return gulp
+    .src(html.output)
+    .pipe(inlineSource())
+    .pipe(gulp.dest(html.dest))
+})
 
 gulp.task('server', () => {
-  connect.server({
+  return connect.server({
     root: ['./dist'],
     livereload: true,
     port: 7777,
-  });
-});
+  })
+})
 
 gulp.task('clean', () => {
-  return del(dirs.dest);
-});
+  return del(dirs.dest)
+})
 
 gulp.task('watch', () => {
-  gulp.watch(html.entry, ['html']);
-  gulp.watch(styles.all, ['styles']);
-});
+  gulp.watch(html.entry, ['html'])
+  gulp.watch(styles.all, ['styles'])
+})
 
 // Default task
 gulp.task('default', ['clean'], () => {
-  gulp.start('html', 'styles', 'server', 'watch');
-});
+  gulp.start('html', 'styles', 'server', 'watch')
+})
 
 // Build
 gulp.task('build', ['clean'], () => {
-  gulp.start('html:production', 'styles:production');
-});
+  gulp.start('html:production', 'styles:production', 'html:inline-styles')
+})
